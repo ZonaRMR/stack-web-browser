7d5b784429be858055cd174dbd95060747241e2e
deeepaaa <depasquale.a@tuta.io>
2016-01-17 18:42:39 +0100

    Fixed bug which restarted the app instantly upon theme change

--- app/src/main/java/eu/depa/browsing/stack/Settings.java
+++ app/src/main/java/eu/depa/browsing/stack/Settings.java
@@ -1,25 +1,22 @@
 package eu.depa.browsing.stack;
 
 import android.app.AlarmManager;
+import android.app.AlertDialog;
 import android.app.PendingIntent;
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
-import android.support.v7.app.AlertDialog;
-import android.util.Log;
 import android.view.ViewConfiguration;
 import android.widget.Toast;
 
 @SuppressWarnings("deprecation")
 
-public class Settings extends PreferenceActivity{
+public class Settings extends PreferenceActivity {
 
     SharedPreferences.OnSharedPreferenceChangeListener ChListener;
 
@@ -31,18 +28,7 @@ public class Settings extends PreferenceActivity{
         ChListener = new SharedPreferences.OnSharedPreferenceChangeListener() {
             public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
                 if (key.equals("theme")) {
-                    final AlertDialog.Builder builder = new AlertDialog.Builder(Settings.this);
-
-                    builder.setTitle(getString(R.string.wanna_restart));
-
-                    builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialogInterface, int i) {
-                            doRestart(getApplicationContext());
-                        }
-                    });
-                    builder.setNegativeButton(getString(R.string.plzNo), null);
-                    builder.show();
+                    askIfRestart();
                 }
             }
         };
@@ -81,6 +67,34 @@ public class Settings extends PreferenceActivity{
         });
     }
 
+    public void askIfRestart () {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(getString(R.string.wanna_restart));
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                doRestart(getApplicationContext(), 100);
+            }
+        });
+        builder.setNegativeButton(getString(R.string.plzNo), null);
+        builder.create();
+        builder.show();
+    }
+
+    public static void doRestart(Context context, int delay) {
+        if (delay == 0)
+            delay = 1;
+        Intent restartIntent = context.getPackageManager()
+                .getLaunchIntentForPackage(context.getPackageName() );
+        PendingIntent intent = PendingIntent.getActivity(
+                context, 0,
+                restartIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+        AlarmManager manager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        manager.set(AlarmManager.RTC, System.currentTimeMillis() + delay, intent);
+        System.exit(2);
+    }
+
     public void setThemeFromPrefs () {
         SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
         switch(sharedPref.getString("theme", "def")) {
@@ -101,33 +115,6 @@ public class Settings extends PreferenceActivity{
                 return;
             case "gray":
                 setTheme(R.style.Gray);
-        }
-    }
-
-    public void doRestart(Context c) {
-        try {
-            if (c != null) {
-                PackageManager pm = c.getPackageManager();
-                if (pm != null) {
-                    Intent mStartActivity = pm.getLaunchIntentForPackage(c.getPackageName());
-                    if (mStartActivity != null) {
-                        mStartActivity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                        int mPendingIntentId = 223344;
-                        PendingIntent mPendingIntent = PendingIntent.getActivity
-                                (c, mPendingIntentId, mStartActivity, PendingIntent.FLAG_CANCEL_CURRENT);
-                        AlarmManager mgr = (AlarmManager) c.getSystemService(Context.ALARM_SERVICE);
-                        mgr.set(AlarmManager.RTC, System.currentTimeMillis() + 1, mPendingIntent);
-
-                        ProgressDialog pd = new ProgressDialog(Settings.this);
-                        pd.setCancelable(false);
-                        pd.setMessage(getString(R.string.restarting));
-                        pd.setProgressStyle(android.R.style.Widget_ProgressBar_Small);
-                        pd.show();
-                    }
-                }
-            }
-        } catch (Exception ex) {
-            Log.e("settings.doRestart", "Was not able to restart application");
         }
     }
 }
